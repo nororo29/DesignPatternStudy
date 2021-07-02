907fb8721e3ddb163c87ab2ec4e8c991d20c2db6
nororo29 <50298349+nororo29@users.noreply.github.com>
2021-07-02 18:15:04 +0900

    Add files via upload

---
+++ DecoratorPattern/RoadDisplay/Client.java
@@ -0,0 +1,23 @@
+package decoratorPattern2;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// 기본 도로 표시
+		Display road = new RoadDisplay();
+		road.draw();
+		
+		// 기본 도로 표시 + 차선표시
+		Display roadWithLane = new LaneDecorator(new RoadDisplay());
+		roadWithLane.draw();
+		
+		// 기본 도로 표시 + 교통량표시
+		Display roadWithTraffic = new TrafficDecorator(new RoadDisplay());
+		roadWithTraffic.draw();
+		
+		// 기본 도로 표시 + 차선표시 + 교통량표시
+		Display roadWithTrafficLane = new TrafficDecorator(new LaneDecorator(new RoadDisplay()));
+		roadWithTrafficLane.draw();
+	}
+
+}
